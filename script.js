4b6cece062bada9e3efff759479a200bbb531974
ilarion <ilaravich@gmail.com>
2024-03-18 22:21:58 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function rndPM() {
+    const min = Math.ceil(1);
+    const max = Math.floor(100);
+    rand_num1 = Math.floor(Math.random() * (max - min + 1) + min);
+    rand_num2 = Math.floor(Math.random() * (max - min + 1) + min);
+    document.getElementById("first").innerHTML = rand_num1;
+    document.getElementById("second").innerHTML = rand_num2;
+}
+function checkresultPM(){
+    let result = document.getElementById("result").value
+    if (result == rand_num1 + rand_num2){
+        document.getElementById("end").innerHTML = "Malacis!"
+    }else if (result != rand_num1 + rand_num2) {
+        document.getElementById("end").innerHTML = "Nepareizi vai atbiles lauks ir tukšs."
+    }
+}
+
+//----------------------------------------------------------------------------------------------->
+
+function rndMD() {
+    const min = Math.ceil(1);
+    const max = Math.floor(20);
+    rand_num1 = Math.floor(Math.random() * (max - min + 1) + min);
+    rand_num2 = Math.floor(Math.random() * (max - min + 1) + min);
+    document.getElementById("first").innerHTML = rand_num1;
+    document.getElementById("second").innerHTML = rand_num2;
+}
+function checkresultMD(){
+    let result = document.getElementById("result").value
+    if (result == rand_num1 + rand_num2){
+        document.getElementById("end").innerHTML = "Malacis!"
+    }else if (result != rand_num1 + rand_num2) {
+        document.getElementById("end").innerHTML = "Nepareizi vai atbiles lauks ir tukšs."
+    }
+}
+
