2971dc97d66b174a7a08a2cab4ced676afd2b856
ilarion <77408321+ilabro@users.noreply.github.com>
2024-03-19 09:39:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function rndPM() {
     const min = Math.ceil(1);
     const max = Math.floor(100);
-    const myArray = ['+', '-'];
-    const rand_element = myArray[Math.floor(Math.random() * myArray.length)]; 
     let rand_num1 = Math.floor(Math.random() * (max - min + 1) + min);
     let rand_num2 = Math.floor(Math.random() * (max - min + 1) + min);
     document.getElementById("first").innerHTML = rand_num1;
     document.getElementById("second").innerHTML = rand_num2;
-    document.getElementById("char").innerHTML = rand_element;
-}
+    const rand_element = ['+', '-'];
+    const iterator = rand_element. values();
+    for (const value of iterator) {
+        document.getElementById("char").innerHTML = rand_element;
+    }
 function checkresultPM(){
     let result = document.getElementById("result").value;
-    let char = document.getElementById("plus").value;
+    let char = document.getElementById("char").value;
     if (char == "+"){
         if (result == rand_num1 + rand_num2){
             document.getElementById("end").innerHTML = "Malacis, pareizi!";
