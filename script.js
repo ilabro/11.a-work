7e6d66e17c67fb824b11943b2adc822ddc060316
ilarion <77408321+ilabro@users.noreply.github.com>
2024-03-26 09:19:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const max = 100
-const min = 1
+const max = 100;
+const min = 1;
 var rand_num1 = Math.floor(Math.random() * (max - min + 1) + min);
 var rand_num2 = Math.floor(Math.random() * (max - min + 1) + min);
+
 function rndPM() {
     if (Math.random()> 0.5){
         document.getElementById("char").innerHTML = "+";
     }else{
         document.getElementById("char").innerHTML = "-";
     }
-    // let randomIndex = Math.floor(Math.random() * strings.length);
-    // let randomString = strings[randomIndex];
-   
     document.getElementById("first").innerHTML = rand_num1;
     document.getElementById("second").innerHTML = rand_num2;
     }
+
 function checkresultPM(){
     var result = document.getElementById("result").value;
     var char = document.getElementById("char").innerHTML;
@@ -36,19 +35,33 @@ function checkresultPM(){
 //----------------------------------------------------------------------------------------------->
 
 function rndMD() {
-    const min = Math.ceil(1);
-    const max = Math.floor(20);
-    rand_num1 = Math.floor(Math.random() * (max - min + 1) + min);
-    rand_num2 = Math.floor(Math.random() * (max - min + 1) + min);
+    if (Math.random()> 0.5){
+        document.getElementById("char").innerHTML = "*";
+    }else{
+        document.getElementById("char").innerHTML = "/";
+    }
     document.getElementById("first").innerHTML = rand_num1;
     document.getElementById("second").innerHTML = rand_num2;
-}
-function checkresultMD(){
-    let result = document.getElementById("result").value
-    if (result == rand_num1 * rand_num2){
-        document.getElementById("end").innerHTML = "Malacis!"
-    }else if (result != rand_num1 * rand_num2) {
-        document.getElementById("end").innerHTML = "Nepareizi vai atbiles lauks ir tukšs."
     }
 }
 
+function checkresultMD(){
+    var result = document.getElementById("result").value;
+    var char = document.getElementById("char").innerHTML;
+    console.log(char)
+    if (char == "*"){
+        if (result == rand_num1 * rand_num2){
+            document.getElementById("board").innerHTML = "Malacis, pareizi!";
+        }else if (result != rand_num1 * rand_num2){
+            document.getElementById("board").innerHTML = "Nepareizi vai atbiles lauks ir tukšs.";
+        }
+    }else if (char == "/"){
+        if (result == rand_num1 / rand_num2){
+            document.getElementById("board").innerHTML = "Malacis, pareizi!";
+        }else if (result != rand_num1 / rand_num2){
+            document.getElementById("board").innerHTML = "Nepareizi vai atbiles lauks ir tukšs.";
+        }
+    }
+}
+
+   
