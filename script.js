b7d332113c6db059c3113c8a8e46cdc256050c29
ilarion <77408321+ilabro@users.noreply.github.com>
2024-04-16 09:43:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -98,21 +98,17 @@ function checkresultMD(){
 }
 
 //----------------------------------------------------------------------------------------------->
-
+var third_grade = [
+    {teksts:"Viens kilograms svaigu dārzeņu maksā 2 eiro, bet 1 kg saldētu dārzeņu maksā 2 reizes dārgāk. Saimniece nopirka 5 kg katra veida dārzeņu. Cik saimniece samaksāja kopā?",atbilde:"30 eiro"},
+    {teksts:"Skolas virtuvei pieveda 100 kg dārzeņu. 44 kg no tiem bija gurķi, bet pārējie bija tomāti. Tomāti bija sasvērti vienādi 7 kastēs. Cik kilogra- mu tomātu bija katrā kastē?",atbilde:"8 katrā kastē"},
+    {teksts:"Kafejnīcai atveda 35 kg burkānu, bet kāpostus par 15 kg vairāk. Saimniece katru dienu izvārīja vienādu daudzumu dārzeņu. Pēc 5 dienām visi dārzeņi bija beigušies. Cik kilogramu katra veida dārzeņu izvārīja katru dienu?",atbilde:"7 burkānus un 10 kāpostus"},
+    {teksts:"Pirmajā un otrajā klasē katrā ir 23 skolēni. Trešajā un ceturtajā klasē katrā ir 26 skolēni. Cik skolēnu ir kopā?",atbilde:"98 skolēni kopā"}
+];
+var uzd3_atbildes_index = Math.floor(Math.random() * (third_grade.length - 0) + 0);
+let uzdevums3klase = third_grade[index];
 function uzd_output3 (){
-    let third_grade = [
-        {teksts:"Viens kilograms svaigu dārzeņu maksā 2 eiro, bet 1 kg saldētu dārzeņu maksā 2 reizes dārgāk. Saimniece nopirka 5 kg katra veida dārzeņu. Cik saimniece samaksāja kopā?",atbilde:"30 eiro"},
-        {teksts:"Skolas virtuvei pieveda 100 kg dārzeņu. 44 kg no tiem bija gurķi, bet pārējie bija tomāti. Tomāti bija sasvērti vienādi 7 kastēs. Cik kilogra- mu tomātu bija katrā kastē?",atbilde:"8 katrā kastē"},
-        {teksts:"Kafejnīcai atveda 35 kg burkānu, bet kāpostus par 15 kg vairāk. Saimniece katru dienu izvārīja vienādu daudzumu dārzeņu. Pēc 5 dienām visi dārzeņi bija beigušies. Cik kilogramu katra veida dārzeņu izvārīja katru dienu?",atbilde:"7 burkānus un 10 kāpostus"},
-        {teksts:"Pirmajā un otrajā klasē katrā ir 23 skolēni. Trešajā un ceturtajā klasē katrā ir 26 skolēni. Cik skolēnu ir kopā?",atbilde:"98 skolēni kopā"}
-    ]
     let izvads = document.getElementById("uzd");
-    let index = Math.floor(Math.random() * (third_grade.length - 0) + 0);
-    console.log(index);
-    let uzdevums = third_grade[index];
-    console.log(uzdevums);
-
-    izvads.innerHTML = uzdevums.teksts;
+    izvads.innerHTML = uzdevums3klase.teksts;
     
 }
 
